c8447807c99e26d0f89b5116d37dcd0d93faae1f
syed vaiz <vaizmehmoodi@gmail.com>
2021-06-03 19:11:13 +0530

    Navbar completed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,16 @@
 import React from 'react'
-import logo from "../logo.jpg";
+import logo from "../sv_logo.jpg";
+//react font awesome import
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 const navbar = () => {
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
           <div className='container'>
 
              <a className="navbar-brand" href="#"><img className='logo' src={logo} alt= "logo..."/></a>
                  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
+                    <FontAwesomeIcon icon={faBars} style={{color: "#ffff"}}/>
                  </button>
 
                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
@@ -19,16 +22,19 @@ const navbar = () => {
                         <a className="nav-link" href="#">About me</a>
                       </li>
                       <li className="nav-item">
-                        <a className="nav-link" href="#">services</a>
+                        <a className="nav-link" href="#">skillset</a>
                       </li>
                       <li className="nav-item">
-                        <a className="nav-link" href="#">experience</a>
+                        <a className="nav-link" href="#">education</a>
                       </li>
                       <li className="nav-item">
-                        <a className="nav-link" href="#">portfolio</a>
+                        <a className="nav-link" href="#">achievements</a>
                       </li>
                       <li className="nav-item">
-                        <a className="nav-link" href="#">contact us</a>
+                        <a className="nav-link" href="#">interest</a>
+                      </li>
+                      <li className="nav-item">
+                        <a className="nav-link" href="#">contact</a>
                       </li>
       
                      </ul>
